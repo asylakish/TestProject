04682c41d6c64b5ba7017a734da3adb8aa31eff8
Akishev <AkishA@kpo.kz>
2023-12-16 10:33:42 +0500

    20231216

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from accounts.models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "is_staff", "is_superuser")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups", "last_login")
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "username", "first_name", "last_name", "password1", "password2"),
+            },
+        ),
+    )
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from accounts.managers import CustomUserManager
+from accounts.validators import validate_name, validate_username
+from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
-    pass
+    username = models.CharField(max_length=50,
+                                unique=True,
+                                help_text=_(
+                                    "Requered. 50 characters or fewer. English letters in lowercase,"
+                                    " or underscore"
+                                ),
+                                validators=[validate_username])
+    email = models.EmailField(unique=True, max_length=255)
+    first_name = models.CharField(max_length=50, validators=[validate_name])
+    last_name = models.CharField(max_length=50, validators=[validate_name])
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects = CustomUserManager()
+
+    def save(self, *args, **kwargs):
+        self.first_name = self.first_name.capitalize()
+        self.last_name = self.last_name.capitalize()
+        self.username = self.username.lower()
+        super().save(*args, **kwargs)
+
+
+
